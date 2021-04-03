5d775d2ca62e2fd87d4592f03331491a1a494d97
PraneethChandra <praneeth.chandra@socketlabs.com>
2021-04-03 13:31:09 -0700

    added retrysettings

---
+++ socketlabs/injectionapi/retrysettings.py
@@ -0,0 +1,40 @@
+from datetime import timedelta
+import random
+
+class RetrySettings(object):
+
+    def __init__(self, maximum_retries = None):
+
+        self.__default_number_of_retries = 0
+        self.__maximum_allowed_number_of_retries = 5
+        self.__minimum_retry_time = timedelta(seconds=1)
+        self.__maximum_retry_time = timedelta(seconds=10)
+
+        if maximum_retries:
+            if maximum_retries < 0:
+                raise ValueError("maximumNumberOfRetries must be greater than 0")
+            if maximum_retries > self.__maximum_allowed_number_of_retries:
+                raise ValueError("The maximum number of allowed retries is ", self.__maximum_allowed_number_of_retries)
+            self.__maximum_number_of_retries = maximum_retries
+        else:
+            self.__maximum_number_of_retries = self.__default_number_of_retries
+        
+    @property
+    def maximum_number_of_retries(self):
+        return self.__maximum_number_of_retries
+    
+    def get_next_wait_interval(self, number_of_attempts):
+        
+        interval = int(min(
+            ((self.__minimum_retry_time.seconds * 1000) + self.get_retry_delta(number_of_attempts)),
+            (self.__maximum_retry_time.seconds * 1000)
+            ))
+        
+        return timedelta(milliseconds=interval)
+    
+    def get_retry_delta(self, number_of_attempts):
+        
+        minimum = (int)((timedelta(seconds=1).seconds * 1000) * 0.8)
+        maximum = (int)((timedelta(seconds=1).seconds * 1000) * 1.2)
+
+        return (int)((pow(2.0, number_of_attempts) - 1.0) * random.randint(minimum, maximum))
